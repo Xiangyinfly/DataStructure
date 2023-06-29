3331dc403b652a23050c7845e9fdce0884788c58
沙丁鱼 <81477590+Xiangyinfly@users.noreply.github.com>
2023-06-29 22:03:58 +0800

    Update SelectSort.java

--- src/com/Sort/SelectSort.java
+++ src/com/Sort/SelectSort.java
@@ -7,7 +7,7 @@ public class SelectSort {
      */
 
     public static void selectSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[i]) {
                     int temp = array[j];
